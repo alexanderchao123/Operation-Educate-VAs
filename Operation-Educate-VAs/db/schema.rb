54d008110cf91684e1721737a420024f9cb3f232
github username <github email address>
2018-12-08 02:04:18 -0500

    hi

--- Operation-Educate-VAs/db/schema.rb
+++ Operation-Educate-VAs/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_08_031146) do
+ActiveRecord::Schema.define(version: 2018_12_08_070052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_12_08_031146) do
     t.string "cumm_nine_elev"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
     t.index ["user_id"], name: "index_questionaires_on_user_id"
   end
 
@@ -48,6 +49,8 @@ ActiveRecord::Schema.define(version: 2018_12_08_031146) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "fname"
+    t.string "lname"
     t.string "va_number"
     t.string "email"
     t.string "password_digest"
