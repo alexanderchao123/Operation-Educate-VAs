c43ee2419f9095175fb8a5b77f69e39eb997d395
github username <github email address>
2018-12-08 09:03:42 -0500

    barebones multistep form

--- Operation-Educate-VAs/app/assets/javascripts/users.js
+++ Operation-Educate-VAs/app/assets/javascripts/users.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let next3 = document.getElementById('next-3')
     let next4 = document.getElementById('next-4')
     let back1 = document.getElementById('back-1')
+    let back2 = document.getElementById('back-2')
+    let back3 = document.getElementById('back-3')
 
     next1.addEventListener('click',function(e){
         e.preventDefault()
@@ -27,6 +29,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
         tab3.classList.add('hide')
         tab4.classList.remove('hide')
     } )
+
+    back1.addEventListener('click', function(e){
+        e.preventDefault()
+        tab2.classList.add('hide')
+        tab1.classList.remove('hide')
+    })
+
+    back2.addEventListener('click', function(e){
+        e.preventDefault()
+        tab2.classList.remove('hide')
+        tab3.classList.add('hide')
+    })
+
+    back3.addEventListener('click', function(e){
+        e.preventDefault()
+        tab3.classList.remove('hide')
+        tab4.classList.add('hide')
+    })
 
 
 })
