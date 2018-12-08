caa393548e67fbb290fc3aad1409e3fb8e72dba9
github username <chao.alexander.123@gmail.com>
2018-12-08 16:24:09 -0500

    modify root path view

--- Operation-Educate-VAs/app/assets/javascripts/users.js
+++ Operation-Educate-VAs/app/assets/javascripts/users.js
@@ -2,14 +2,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let tab1 = document.getElementById("tab-1")
     let tab2 = document.getElementById("tab-2")
     let tab3 = document.getElementById("tab-3")
-    // let tab4 = document.getElementById("tab-4")
     let next1 = document.getElementById('next-1')
     let next2 = document.getElementById('next-2')
-    // let next3 = document.getElementById('next-3')
-    // let next4 = document.getElementById('next-4')
     let back1 = document.getElementById('back-1')
     let back2 = document.getElementById('back-2')
-    // let back3 = document.getElementById('back-3')
 
     next1.addEventListener('click',function(e){
         e.preventDefault()
@@ -23,12 +19,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
         tab3.classList.remove('hide')
     } )
 
-    // next3.addEventListener('click',function(e){
-    //     e.preventDefault()
-    //     tab3.classList.add('hide')
-    //     tab4.classList.remove('hide')
-    // } )
-
     back1.addEventListener('click', function(e){
         e.preventDefault()
         tab2.classList.add('hide')
@@ -40,11 +30,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
         tab2.classList.remove('hide')
         tab3.classList.add('hide')
     })
-
-    // back3.addEventListener('click', function(e){
-    //     e.preventDefault()
-    //     tab3.classList.remove('hide')
-    //     tab4.classList.add('hide')
-    // })
 
 })
